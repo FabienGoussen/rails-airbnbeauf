18bad75015f1c3b89e7e7098dc3c823cf67e7f89
Leo Vannier <leovannier@sent.com>
2016-06-08 16:26:23 +0200

    tentative booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,15 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  def self.book
+    SELECT product_id FROM products R
+    WHERE RoomType = 'Single' AND NOT EXISTS
+    (SELECT * FROM BOOKINGS
+     WHERE product_id = R.product_id
+        AND begin_date >= '//'
+        AND end_date <= '//'
+    )
+    where("begin_date <= ?", "#{search}")
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,16 +2,22 @@ class Product < ActiveRecord::Base
 
   mount_uploader :picture, PictureUploader
   belongs_to :user
+  has_many :users, through: :bookings
+  has_many :bookings
 
   def self.city_search(search)
     where("city LIKE ?", "%#{search}%")
   end
 
   def self.begin_date_search(search)
-    where("begin_date = ?", "#{search}")
+    where("begin_date >= ?", "#{search}")
   end
 
   def self.end_date_search(search)
-    where("end_date = ?", "#{search}")
+    where("end_date <= ?", "#{search}")
+  end
+
+  def self.book(a, b)
+    where("begin_date ")
   end
 end
