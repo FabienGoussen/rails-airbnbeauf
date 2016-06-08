7409198965985260983f559a161ee735d03e37eb
Leo Vannier <leovannier@sent.com>
2016-06-08 14:19:45 +0200

    search ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,15 @@ class Product < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   belongs_to :user
 
-  def self.search(search)
+  def self.city_search(search)
     where("city LIKE ?", "%#{search}%")
+  end
+
+  def self.begin_date_search(search)
+    where("begin_date = ?", "#{search}")
+  end
+
+  def self.end_date_search(search)
+    where("end_date = ?", "#{search}")
   end
 end
