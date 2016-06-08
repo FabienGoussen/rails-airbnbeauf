8a47029b9b9b8498f53cbbdce60ee377c90da674
fabien goussen <goussen.fabien@gmail.com>
2016-06-08 18:36:55 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
  # 5.times do
-  post = User.new({
-                    first_name: Faker::Name.first_name,
-                    last_name: Faker::Name.last_name,
-                    age: Faker::Number.between(18, 80),
-                    email: Faker::Internet.email,
-                    password: Faker::Internet.password,
-                    message: Faker::Lorem.characters,
-                    phone_number: Faker::PhoneNumber.phone_number
-                    })
-  post.save
-   post4 = User.new({
-                    first_name: Faker::Name.first_name,
-                    last_name: Faker::Name.last_name,
-                    age: Faker::Number.between(18, 80),
-                    email: Faker::Internet.email,
-                    password: Faker::Internet.password,
-                    message: Faker::Lorem.characters,
-                    phone_number: Faker::PhoneNumber.phone_number
-                    })
-  post4.save
-# end
 
-# 5.times do
-  post2 = Product.new({
-                    country: Faker::Address.country,
-                    city: Faker::Address.city,
-                    name: Faker::Name.name,
-                    address: "12 rue des Tours Lille",
-                    description: Faker::Lorem.sentence,
-                    price: Faker::Commerce.price,
-                    begin_date: Faker::Date.forward(23),
-                    end_date: Faker::Date.backward(14)
-                    })
-  post2.save
-post3 = Product.new({
-                    country: Faker::Address.country,
-                    city: Faker::Address.city,
-                    name: Faker::Name.name,
-                    address: "48 rue de Douai Lille",
-                    description: Faker::Lorem.sentence,
-                    price: Faker::Commerce.price,
-                    begin_date: Faker::Date.forward(23),
-                    end_date: Faker::Date.backward(14)
-                    })
-  post3.save
-# end
