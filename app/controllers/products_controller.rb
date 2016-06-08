0d7d88cd345287fbebc8de295e20dbf49603aec6
fabien goussen <goussen.fabien@gmail.com>
2016-06-08 19:01:06 +0200

    booking view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,7 +58,7 @@ class ProductsController < ApplicationController
    # start_date = params[:start_date].to_date.beginning_of_day
    # end_date = params[:end_date].to_date.end_of_day
    # records = Campaign.where(:created_at => start_date..end_date)
-   end
+  end
 
     private
   #
