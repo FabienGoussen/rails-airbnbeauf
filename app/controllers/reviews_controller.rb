b145723b474e3837f98d75dcee8cd2577463281b
Leo Vannier <leovannier@sent.com>
2016-06-10 14:59:55 +0200

    revoews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @review = Review.new(review_params)
-    @review.product = @product
+    @review.product_id = @product
     if @review.save
       redirect_to product_path(@product)
     else
